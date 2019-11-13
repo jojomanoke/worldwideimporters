94643c927c264df0d87021dac08d05e23184eb2a
sylvano <sylvano.verkuyl@nugtr.nl>
2019-11-13 13:36:27 +0100

    Added placeholder to products

--- resources/pages/categories/show.php
+++ resources/pages/categories/show.php
@@ -7,21 +7,23 @@ $port = DATABASE_PORT;
 
 
 $connection = new mysqli($host, $user, $pass, $databasename, $port);
-$category = $_GET['category'];
+$category = $_GET[ 'category' ];
 $query = "SELECT * FROM stockitems WHERE StockItemID IN (SELECT StockItemID FROM stockitemstockgroups WHERE StockGroupID = $category);";
 $results = $connection->query($query);
 ?>
-
-<table>
-    <tbody>
-    <?php while($row = $results->fetch_assoc()){ ?>
-        <tr>
-            <td><?php echo $row['StockItemName'] ?></td>
-        </tr>
-    <?php } ?>
-    </tbody>
-</table>
-
+    <div class="row">
+        <?php while ( $row = $results->fetch_assoc() ) { ?>
+            <div class="col-4 my-3">
+                <div class="card">
+                    <img src="<?php if(isset($row[ 'photo' ])) {echo $row['photo'];} else {echo 'https://via.placeholder.com/350x200';} ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $row[ 'StockItemName' ]; ?></h5>
+                        <p class="card-text"><?php echo $row[ 'SearchDetails' ]; ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
 <?php
 $results->free();
 $connection->close();
