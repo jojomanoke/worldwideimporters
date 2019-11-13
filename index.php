d1f4a23f4d61ce655126018b53f35a7a4676a955
sylvano <sylvano.verkuyl@nugtr.nl>
2019-11-13 11:33:59 +0100

    Added clean URL's

--- index.php
+++ index.php
@@ -4,7 +4,12 @@ include( 'resources/helpers/functions.php' );
 include( 'resources/includes/head.php' );
 include( 'resources/includes/navbar.php' );
 
-
+switch(activeUrl()){
+    default:
+    case '/home':
+        include 'resources/pages/home.php';
+        break;
+}
 
 
 
---
+++ resources/pages/home.php
@@ -0,0 +1,5 @@
+<div class="table-bordered">
+    <table>
+    
+    </table>
+</div>
