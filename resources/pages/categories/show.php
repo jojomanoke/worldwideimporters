535f1af0a20e24db6f986fac15472bffa880df6a
Bas <klaauwb@xs4all.nl>
2019-11-14 13:30:00 +0100

    Pagination en resultaten per pagina.

--- resources/pages/categories/show.php
+++ resources/pages/categories/show.php
@@ -4,26 +4,80 @@ $databasename = DATABASE;
 $user = DATABASE_USER;
 $pass = DATABASE_PASSWORD; //eigen password invullen
 $port = DATABASE_PORT;
+if (isset($_POST['ARPP'])) {
+    $resultsPerPage = $_POST['ARPP'];
+} else {
+    $resultsPerPage = 25;
+}
 
-
+$currentPage = isset($_GET['pagenumber']) ? $_GET['pagenumber'] : 1;
+$thisPageFirstResult = ($currentPage - 1) * $resultsPerPage;
 $connection = new mysqli($host, $user, $pass, $databasename, $port);
-$category = $_GET[ 'category' ];
-$query = "SELECT * FROM stockitems WHERE StockItemID IN (SELECT StockItemID FROM stockitemstockgroups WHERE StockGroupID = $category);";
+$category = $_GET['category'];
+$query = "SELECT * FROM stockitems WHERE StockItemID IN (SELECT StockItemID FROM stockitemstockgroups WHERE StockGroupID = $category) LIMIT $thisPageFirstResult, $resultsPerPage;";
 $results = $connection->query($query);
+
+
+$queryTwo = "SELECT * FROM stockitems WHERE StockItemID IN (SELECT StockItemID FROM stockitemstockgroups WHERE StockGroupID = $category)";
+$numberOfResults = $connection->query($queryTwo)->num_rows;
+
+$numberOfPages = ceil($numberOfResults / $resultsPerPage);
+//for ($page = 1; $page <= $numberOfPages; $page++) {
+//    echo '<a href="?pagenumber=' . $page . '">' . $page . '</a>';
+//}
+
 ?>
-    <div class="row">
-        <?php while ( $row = $results->fetch_assoc() ) { ?>
+    <form id="perPage" class="form-inline mt-5 pt-5" method="post">
+        <select class="form-control w-auto" name="ARPP">
+            <option <?php if(isset($_POST['ARPP']) && $_POST['ARPP'] == 25){ echo "selected";} ?> value=25>25</option>
+            <option <?php if(isset($_POST['ARPP']) && $_POST['ARPP'] == 50){ echo "selected";} ?> value=50>50</option>
+            <option <?php if(isset($_POST['ARPP']) && $_POST['ARPP'] == 100){ echo "selected";} ?> value=100>100</option>
+        </select>
+        <button class="btn btn-primary" type="submit">Aantal Resultaten</button>
+    </form>
+
+
+    <div class="row mb-5">
+
+        <?php while ($row = $results->fetch_assoc()) { ?>
             <div class="col-4 my-3">
                 <div class="card">
-                    <img src="<?php if(isset($row[ 'photo' ])) {echo $row['photo'];} else {echo 'https://via.placeholder.com/350x200';} ?>" class="card-img-top" alt="...">
+                    <img src="<?php if (isset($row['photo'])) {
+                        echo $row['photo'];
+                    } else {
+                        echo 'https://via.placeholder.com/350x200';
+                    } ?>" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $row[ 'StockItemName' ]; ?></h5>
-                        <p class="card-text"><?php echo $row[ 'SearchDetails' ]; ?></p>
+                        <h5 class="card-title"><?php echo $row['StockItemName']; ?></h5>
+                        <p class="card-text"><?php echo $row['SearchDetails']; ?></p>
                     </div>
                 </div>
             </div>
         <?php } ?>
     </div>
+
+
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-center ">
+                        <?php if(!($currentPage<=1)){?>
+                        <li class="page-item"><a class="page-link" href="<?php echo $currentPage-1;?>">Previous</a></li>
+                        <?php } ?>
+                        <?php for ($page = 1; $page <= $numberOfPages; $page++) { ?>
+                            <li class="page-item<?php if ($page == $currentPage) {
+                                echo ' active';
+                            } else {
+                                echo '';
+                            } ?>">
+                                <a class="page-link" href="?pagenumber=<?php echo $page; ?>">
+                                    <?php echo $page; ?>
+                                </a>
+                            </li>
+                        <?php } ?>
+                        <?php if(!($currentPage>=$numberOfPages)){?>
+                        <li class="page-item"><a class="page-link" href="?pagenumber=<?php echo $currentPage+1;?>">Next</a></li>
+                        <?php }?>
+                    </ul>
+                </nav>
 <?php
 $results->free();
 $connection->close();
