dd6aef90b4ed1a9300fc70c4330b815b7f16cb72
sylvano <sylvano.verkuyl@nugtr.nl>
2019-11-15 13:08:01 +0100

    Fixed

--- resources/classes/DB.php
+++ resources/classes/DB.php
@@ -3,23 +3,16 @@
 
 class DB
 {
-    /**
-     * @var $db DB Current class
-     */
     private static $db;
-    
-    /**
-     * @var \MySQLi Connection
-     */
     private $connection;
     
     private function __construct()
     {
         $this->connection = new MySQLi(
-            env('DATABASE_HOST', '127.0.0.1'),
-            env('DATABASE_USER', 'root'),
+            'localhost',
+            env('DATABASE_USER'),
             env('DATABASE_PASSWORD', ''),
-            env('DATABASE_NAME', 'wideworldimporters')
+            env('DATABASE_NAME')
         );
     }
     
