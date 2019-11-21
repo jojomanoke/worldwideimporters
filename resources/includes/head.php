c5ab36e2479782a4207b687a88dd605315980a8b
MelihAlex10 <55081150+MelihAlex10@users.noreply.github.com>
2019-11-21 21:12:21 +0100

    een leuk favicon

--- resources/includes/head.php
+++ resources/includes/head.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <title><?=env('WEBSITE_NAME')?></title>
     <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="shortcut icon" type="image/x-icon" href="/images/Wideworldimporters.png" sizes="32x32">
 </head>
 <body>
