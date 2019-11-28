428dd595bbe41026dcc62ed8ca71cb999eda86fd
MelihAlex10 <55081150+MelihAlex10@users.noreply.github.com>
2019-11-28 09:02:53 +0100

    inlog,register,favicon fix

---
+++ resources/includes/login.php
@@ -0,0 +1,48 @@
+<?php
+// session_start();
+$msg = "";
+
+if (isset($_POST['submit'])) {
+    $connection = new mysqli('localhost', 'root', '', 'users');
+    $email = $connection->real_escape_string($_POST['email']);
+    $password = $connection->real_escape_string($_POST['wachtwoord']);
+
+    $sql = $connection->query("SELECT wachtwoord FROM users WHERE email='$email'");
+    if ($sql->num_rows > 0) {
+        $data = $sql->fetch_array();
+        if (password_verify($password, $data['wachtwoord'])) {
+            // $_SESSION['userSession'] = $data['email'];
+            $msg = "Je bent ingelogd!";
+        } else
+            $msg = "Controleer uw gegevens!";
+    } else
+        $msg = "Controleer je gegevens!";
+
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0">
+    <meta http-equiv="X-UA-Compitable" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<div class="container" style="margin-top: 100px;">
+    <div class="row justify-content-center">
+        <div class="col-md-6 col-md-offset-3" align="center"><br><br>
+            <?php if ($msg !="") echo $msg . "<br><br>"; ?>
+            <h1>Inloggen</h1>
+            <i class="material-icons">lock</i>
+            <br>
+            <div>
+
+            <form method="post" action="login.php">
+            <input class="form-control" type="text" name="email" placeholder="E-mail..."><br>
+            <input class="form-control" type="password" name="wachtwoord" placeholder="Wachtwoord..."><br>
+            <input type="submit" class="btn btn-primary" value="Log in!" name="submit">
+            </form>
+            </div>
--- resources/includes/navbar.php
+++ resources/includes/navbar.php
@@ -40,11 +40,11 @@
             <a href="/shoppingcart"
                class="nav-link <?php if(getUrl() === '/shoppingcart') echo 'active'; ?>"><i class="material-icons-outlined" style="color: black">shopping_basket</i></a>
             <div class="dropdown show">
-                <a class="nav-link dropdown-toggle " href="" role="button" id="profiel" data-toggle="dropdown"
+                <a class="nav-link dropdown-toggle" href="" role="button" id="profiel" data-toggle="dropdown"
                    aria-haspopup="true" aria-expanded="false"><i class="material-icons">person</i></a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="profiel">
-                    <a class="dropdown-item" href="">Inloggen</a>
-                    <a class="dropdown-item" href="">Registreren</a>
+                    <a class="dropdown-item" href=/resources/includes/login.php>Inloggen</a>
+                    <a class="dropdown-item" href=/resources/includes/register.php>Registreren</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="">Mijn profiel</a>
                 </div>
