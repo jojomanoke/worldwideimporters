56f1ca848f0f92856c8a53023da5ad47c3c48475
Bas <klaauwb@xs4all.nl>
2019-11-28 09:27:22 +0100

    Shoppingcart

--- resources/pages/shoppingcart.php
+++ resources/pages/shoppingcart.php
@@ -1,7 +1,18 @@
 <?php
-if($_POST['product'] && $_GET['action'] === 'add'){
-    $shoppingCart = $_SESSION[ 'shoppingcart' ] ?? [];
-    $shoppingCart[] = $_POST[ 'product' ];
+$shoppingCart = $_SESSION['shoppingcart'] ?? [];
+if (isset($_POST['product']) && $_GET['action'] === 'add') {
+    if (!in_array($_POST['product'], $shoppingCart)) {
+        $shoppingCart[] = $_POST['product'];
+        $_SESSION['shoppingcart'] = $shoppingCart;
+    }
 }
+$conn = \Classes\Database::getConnection();
 
-dd($shoppingCart);
+$query = "SELECT * FROM stockitems WHERE StockItemID IN (" .implode(', ', $shoppingCart). ')';
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$results = $stmt->get_result();
+while($product = $results->fetch_object()){ ?>
+    <p><?= $product->StockItemName ?></p>
+    <?php } ?>
+
