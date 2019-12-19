e0b87eff589de2c28a571130768ca98d24712271
sylvano <sylvano.verkuyl@nugtr.nl>
2019-12-19 09:40:57 +0100

    Fix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-if (!isset($_SESSION)) {
+if(!isset($_SESSION)) {
     session_start();
     $_SESSION['language'] = 'en';
 }
@@ -10,19 +10,19 @@ include('resources/config/globals.php');
 include('resources/includes/head.php');
 $shoppingCart = $_SESSION['shoppingcart'] ?? [];
 //dd($_POST, $_GET, !in_array($_POST[ 'product' ], array_column($shoppingCart, 'id'), false));
-if ( isset($_GET[ 'action' ], $_POST[ 'product' ]) && $_GET[ 'action' ] === 'add' && !in_array($_POST[ 'product' ], array_column($shoppingCart, 'id'), false)) {
+if(isset($_GET['action'], $_POST['product']) && $_GET['action'] === 'add' && !in_array($_POST['product'], array_column($shoppingCart, 'id'), false)) {
     $shoppingCart[] = [
-        'id' => (int)$_POST['product'],
+        'id'     => (int)$_POST['product'],
         'amount' => 1,
     ];
 //    dd($shoppingCart);
     $_SESSION['shoppingcart'] = array_merge($shoppingCart);
-    echo '<script>window.location.href="'.getUrl().'"</script>';
+    echo '<script>window.location.href="' . getUrl() . '"</script>';
 }
 
-if ( isset($_POST[ 'product' ], $_GET['action']) && $_GET[ 'action' ] === 'delete' && $_GET['page'] === 'shoppingcart' && in_array((int)$_POST[ 'product' ], array_column($shoppingCart, 'id'), false) ) {
-    foreach ($shoppingCart as $key => $item) {
-        if ($item['id'] === (int)$_POST['product']) {
+if(isset($_POST['product'], $_GET['action']) && $_GET['action'] === 'delete' && $_GET['page'] === 'shoppingcart' && in_array((int)$_POST['product'], array_column($shoppingCart, 'id'), false)) {
+    foreach($shoppingCart as $key => $item) {
+        if($item['id'] === (int)$_POST['product']) {
             unset($shoppingCart[$key]);
         }
     }
@@ -36,44 +36,45 @@ include('resources/includes/navbar.php');
             <?php
             include('resources/includes/categoryBar.php');
             ?><?php
-            switch ($_GET['page']) {
+            switch($_GET['page']) {
                 default:
+                case 'home':
                 case 'categories':
                     include 'resources/pages/categories/show.php';
                     break;
-
+                
                 case 'products':
                     include 'resources/pages/products/show.php';
                     break;
-
+                
                 case 'editProduct':
                     include 'resources/pages/products/edit.php';
                     break;
-
+                
                 case 'shoppingcart':
                     include 'resources/pages/shoppingcart.php';
                     break;
-
+                
                 case 'search':
                     include 'resources/pages/search.php';
                     break;
-
+                
                 case 'login':
                     include 'resources/pages/auth/login.php';
                     break;
-
+                
                 case 'register':
                     include 'resources/pages/auth/register.php';
                     break;
-
+                
                 case 'logout':
                     include 'resources/pages/auth/logout.php';
                     break;
-
+                
                 case 'checkout':
                     include 'resources/pages/checkout/checkout.php';
                     break;
-    
+                
                 case 'profile':
                     include 'resources/pages/auth/profile.php';
                     break;
